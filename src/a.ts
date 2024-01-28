7f443d32f6b0ec62b09e1c157b658600edbd2383
Sourin <iamsourinsarkar@gmail.com>
2024-01-28 19:53:13 +0530

    Optimised tsconfig

---
+++ src/a.ts
@@ -0,0 +1,29 @@
+// const x: number = 1;
+// console.log(x);
+
+// function greet(firstName: string)
+// {
+//     console.log("Hello " + firstName);
+// }
+
+// greet("Sourin");
+
+// function sum(num1: number, num2: number): number {
+//     return(num1 + num2);
+// }
+
+// console.log("Sum: " + sum(1,2));
+
+// function isLegal(inputAge: number): boolean {
+//     if(inputAge > 18) return true;
+//     else return false;
+// }
+
+// function runAfter1s(fn: () => void) {
+//     setTimeout(fn, 1000);
+// };
+
+// runAfter1s(() => {
+//     console.log("Hi");
+// });
+
